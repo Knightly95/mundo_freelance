36131133acc0c5973a52baad6d02a0e3b399b7cc
Luis Villarroel <ldvg95@outlook.com>
2017-05-08 07:16:41 -0400

    registro y login

--- js/app.js
+++ js/app.js
@@ -1,10 +1,77 @@
-var app = angular.module("mundo_freelance",[])
+var app = angular.module("mundo_freelance",['ui.router'])
 
-.controller("main", ["$scope",function($scope){
+.controller("mainCtrl", ["$scope",function($scope){
 	$scope.aboutus = true;
 
 	$scope.showAboutUs = function(){
 		$scope.aboutus = true;
 	};
+}])
+.controller("navbarCtrl", ["$scope",function($scope){
+	$scope.aboutus = true;
 
+	$scope.showAboutUs = function(){
+		$scope.aboutus = true;
+	};
+}])
+.controller("registroCtrl", ["$scope",function($scope){
+	$scope.options1 = [
+	{"id":"1", "name":"Ninguno"},
+	{"id":"2", "name":"Basico"},
+	{"id":"3", "name":"Medio"},
+	{"id":"4", "name":"Avanzado"}
+	];
+	$scope.options2 = [
+	{"id":"1", "name":"Clase A (Alta/Vip)"},
+	{"id":"1", "name":"Clase B (Avanzado)"},
+	{"id":"1", "name":"Clase C (Medio)"},
+	{"id":"1", "name":"Clase D (Baja)"}
+	];
+}])
+.config(["$stateProvider","$urlRouterProvider",
+	function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.when("/", "/home");
+  $urlRouterProvider.when("", "/home");
+  $urlRouterProvider.otherwise("/home");
+
+  // Now set up the states
+  $stateProvider
+    .state("base",{
+      abstract: true,
+      views:{
+        "":{
+          templateUrl: "partials/base.html",
+        },
+        "navbar@base": {
+          templateUrl : "partials/navbar.html",
+          controller: "navbarCtrl",
+        }
+      },
+    })
+    .state('base.home', {
+      url: "/home",
+      views : {
+        "page@base":{
+          templateUrl : "partials/home.html",
+          controller: "mainCtrl",
+        }
+      }
+    })
+    .state('base.ingreso', {
+      url: "/ingreso",
+      views : {
+        "page@base":{
+          templateUrl : "partials/ingreso.html"
+        }
+      }
+    })
+    .state('base.registro', {
+      url: "/registro",
+      views : {
+        "page@base":{
+          templateUrl : "partials/registro.html",
+          controller: "registroCtrl"
+        }
+      }
+    });
 }]);
